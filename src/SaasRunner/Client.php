02acccdfbac455c3dbf629c495a93fff8975bb18
Paul Springett <paul@springett.me>
2014-02-12 16:00:05 +0000

    Use standardised documentation

--- src/SaasRunner/Client.php
+++ src/SaasRunner/Client.php
@@ -4,34 +4,38 @@ namespace SaasRunner;
 
 use Guzzle;
 
-# SaasRunner\Client
-#
-# Responsible for sending requests and processing responses from the API.
-# Delegates resources to the relevant Resource class.
-#
-# Example:
-#
-#   $client = new SaasRunner\Client('5201bad4-014a-4eb6-b18c-35b18c6b4249');
-#
-#   $client->subscribers->create(['subscriber_uid' => 'SB#012743']);
-#   // array(
-#   //   'subscriber' => array(
-#   //     'id' => 5065,
-#   //     'subscriber_uid' => 'SB#012743',
-#   //     'dated_at' => '2014-02-12',
-#   //     'meta' => NULL
-#   //   )
-#   // )
-#
-class Client {
-
-    # Public: create a new instance of SaasRunner\Client
-    #
-    #   string $apiKey  - Your Saas Runner dashboard API key
-    #   string $apiHost - Alternate API hostname ("api.saarunner.com")
-    #
-    # Returns instance of SaasRunner\Client
-    public function __construct($apiKey, $apiHost = 'api.saasrunner.com') {
+/**
+* SaasRunner\Client
+*
+* Responsible for sending requests and processing responses from the API.
+* Delegates resources to the relevant Resource class.
+*
+* Example:
+*
+*   $client = new SaasRunner\Client('5201bad4-014a-4eb6-b18c-35b18c6b4249');
+*
+*   $client->subscribers->create(['subscriber_uid' => 'SB*012743']);
+*   // array(
+*   //   'subscriber' => array(
+*   //     'id' => 5065,
+*   //     'subscriber_uid' => 'SB*012743',
+*   //     'dated_at' => '2014-02-12',
+*   //     'meta' => NULL
+*   //   )
+*   // )
+*/
+class Client
+{
+    /**
+    * Create a new instance of SaasRunner\Client
+    *
+    * @param string $apiKey Your Saas Runner dashboard API key
+    * @param string $apiHost Alternate API hostname ("api.saarunner.com")
+    *
+    * @return SaasRunner\Client
+    */
+    public function __construct($apiKey, $apiHost = 'api.saasrunner.com')
+    {
         $this->client = new Guzzle\Http\Client('http://' . $apiHost);
 
         $this->setApiKey($apiKey);
@@ -41,43 +45,55 @@ class Client {
         $this->events       = new \SaasRunner\Resources\Event($this);
     }
 
-    # Public: perform an HTTP GET request
-    #
-    #   string $path - URL path to send the request to
-    #
-    # Return instance of Guzzle\Http\Message\Response
-    public function get($path) {
+    /**
+    * Perform an HTTP GET request
+    *
+    * @return string $path URL path to send the request to
+    *
+    * @return Guzzle\Http\Message\Response
+    */
+    public function get($path)
+    {
         return $this->request('get', $path);
     }
 
-    # Public: perform an HTTP POST request
-    #
-    #   string $path - URL path to send the request to
-    #   array $params - Parameters to send as the POST body
-    #
-    # Return instance of Guzzle\Http\Message\Response
-    public function post($path, $params = []) {
+    /**
+    * Perform an HTTP POST request
+    *
+    * @param string $path URL path to send the request to
+    * @param array $params Parameters to send as the POST body
+    *
+    * @return Guzzle\Http\Message\Response
+    */
+    public function post($path, $params = [])
+    {
         return $this->request('post', $path, $params);
     }
 
-    # Public: perform an HTTP DELETE request
-    #
-    #   string $path - URL path to send the request to
-    #
-    # Return instance of Guzzle\Http\Message\Response
-    public function delete($path) {
+    /**
+    * Perform an HTTP DELETE request
+    *
+    * @param string $path URL path to send the request to
+    *
+    * @return Guzzle\Http\Message\Response
+    */
+    public function delete($path)
+    {
         return $this->request('delete', $path);
     }
 
-    # Internal: perform an HTTP request
-    #
-    #   string $httpMethod - HTTP request method to perform (eg. "post")
-    #   string $path - URL path to send the request to
-    #   array $params - Parameters to send as the POST body
-    #
-    # Throws SaasRunner\Exception\ResponseError for HTTP response starting 4xx
-    # Return instance of Guzzle\Http\Message\Response
-    protected function request($httpMethod, $path, $params = []) {
+    /**
+    * Perform an HTTP request
+    *
+    * @param string $httpMethod HTTP request method to perform (eg. "post")
+    * @param string $path URL path to send the request to
+    * @param array $params Parameters to send as the POST body
+    *
+    * @throws SaasRunner\Exception\ResponseError for HTTP response starting 4xx
+    * @return Guzzle\Http\Message\Response
+    */
+    protected function request($httpMethod, $path, $params = [])
+    {
         $request = $this->client->createRequest($httpMethod, $path, [], $params, []);
 
         try {
@@ -95,12 +111,15 @@ class Client {
         return $response;
     }
 
-    # Internal: store the API key and add it as a default header
-    #
-    #   string $apiKey  - The API key to store
-    #
-    # Returns this
-    protected function setApiKey($apiKey) {
+    /**
+    * Store the API key and add it as a default header
+    *
+    * @param string $apiKey The API key to store
+    *
+    * @return $this
+    */
+    protected function setApiKey($apiKey)
+    {
         $this->apiKey = $apiKey;
         $this->client->setDefaultOption('headers', ['X-API-Key' => $apiKey]);
 
--- src/SaasRunner/Resources/Event.php
+++ src/SaasRunner/Resources/Event.php
@@ -2,50 +2,61 @@
 
 namespace SaasRunner\Resources;
 
-# SaasRunner\Resources\Event
-#
-# Responsible managing Event resources
-#
-# Example:
-#
-#   $client = new SaasRunner\Client('5201bad4-014a-4eb6-b18c-35b18c6b4249');
-#   $client->events->index();
-#
-class Event {
-
-    # Public: create a new instance of SaasRunner\Resources\Event
-    #
-    #   Client $client - SaasRunner\Client with API key
-    #
-    # Returns instance of SaasRunner\Resources\Event
-    public function __construct(\SaasRunner\Client $client) {
+/**
+* SaasRunner\Resources\Event
+*
+* Responsible managing Event resources
+*
+* Example:
+*
+*   $client = new SaasRunner\Client('5201bad4-014a-4eb6-b18c-35b18c6b4249');
+*   $client->events->index();
+*/
+class Event
+{
+    /**
+    * Create a new instance of SaasRunner\Resources\Event
+    *
+    * @param Client $client SaasRunner\Client with API key
+    */
+    public function __construct(\SaasRunner\Client $client)
+    {
         $this->client = $client;
     }
 
-    # Public: attempt to retrieve all events
-    #
-    # Returns an Array
-    public function index() {
+    /**
+    * Attempt to retrieve all events
+    *
+    * @return array
+    */
+    public function index()
+    {
         $response = $this->client->get('/events');
         return $response->json();
     }
 
-    # Public: attempt to retrieve a single event
-    #
-    #   integer $id - event id
-    #
-    # Returns an Array
-    public function show($id) {
+    /**
+    * Attempt to retrieve a single event
+    *
+    * @param integer $id event id
+    *
+    * @return array
+    */
+    public function show($id)
+    {
         $response = $this->client->get("/events/$id");
         return $response->json();
     }
 
-    # Public: attempt to destroy a single event
-    #
-    #   integer $id - event id
-    #
-    # Returns an Array
-    public function destroy($id) {
+    /**
+    * Attempt to destroy a single event
+    *
+    * @param integer $id event id
+    *
+    * @return array
+    */
+    public function destroy($id)
+    {
         $response = $this->client->delete("/events/$id");
         return $response->json();
     }
