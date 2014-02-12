5964e183d94398fca034a22af04c053df69890a6
Paul Springett <paul@springett.me>
2014-02-12 13:33:10 +0000

    Add request methods to client

--- src/SaasRunner/Client.php
+++ src/SaasRunner/Client.php
@@ -11,8 +11,53 @@ class Client {
         $this->setApiKey($apiKey);
     }
 
+    # Public: perform an HTTP GET request
+    #
+    #   string $path - URL path to send the request to
+    #
+    # Return instance of Guzzle\Http\Message\Response
+    public function get($path) {
+        return $this->request('get', $path);
+    }
 
+    # Public: perform an HTTP POST request
+    #
+    #   string $path - URL path to send the request to
+    #   array $params - Parameters to send as the POST body
+    #
+    # Return instance of Guzzle\Http\Message\Response
+    public function post($path, $params = []) {
+        return $this->request('post', $path, $params);
+    }
 
+    # Public: perform an HTTP DELETE request
+    #
+    #   string $path - URL path to send the request to
+    #
+    # Return instance of Guzzle\Http\Message\Response
+    public function delete($path) {
+        return $this->request('delete', $path);
+    }
+
+    # Internal: perform an HTTP request
+    #
+    #   string $httpMethod - HTTP request method to perform (eg. "post")
+    #   string $path - URL path to send the request to
+    #   array $params - Parameters to send as the POST body
+    #
+    # Return instance of Guzzle\Http\Message\Response
+    protected function request($httpMethod, $path, $params = []) {
+        $request = $this->client->createRequest($httpMethod, $path, [], $params, []);
+        $response = $request->send();
+
+        return $response;
+    }
+
+    # Internal: store the API key and add it as a default header
+    #
+    #   string $apiKey  - The API key to store
+    #
+    # Returns this
     protected function setApiKey($apiKey) {
         $this->apiKey = $apiKey;
         $this->client->setDefaultOption('headers', ['X-API-Key' => $apiKey]);
